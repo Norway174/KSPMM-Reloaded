95b08a817ff972e3481b29fc3af8468262a71695
Norway174 <willyodden@hotmail.com>
2013-11-07 01:23:57 +0100

    Alpha #1.01
    Added the Updater files from KSPMM, and updated the Todo.txt with a
    message to RolfCopter.

---
+++ KSPMM Reloaded/KSPMM Reloaded/Updater.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Updater
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 12)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(409, 23)
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Enabled = False
+        Me.Button1.Location = New System.Drawing.Point(12, 41)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Cancel"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(93, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Status"
+        '
+        'Updater
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(433, 74)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Updater"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "Updating Kerbal Space Program Mod Manager"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+End Class
---
+++ KSPMM Reloaded/KSPMM Reloaded/Updater.vb
@@ -0,0 +1,76 @@
+﻿Imports System.Net
+
+Public Class Updater
+    Dim Cancel As Boolean = False
+    Dim Done As Boolean = False
+
+    Dim mainExe As String = System.IO.Path.GetFileName(Application.ExecutablePath)
+    Dim oldExeLoc As String = System.IO.Path.GetDirectoryName(mainExe)
+    Dim oldExe As String = oldExeLoc & "KSPMM.Old"
+
+
+    Private _passedText As String
+
+    Public Property [PassedText]() As String
+        Get
+            Return _passedText
+        End Get
+        Set(ByVal Value As String)
+            _passedText = Value
+        End Set
+    End Property
+
+    Dim client As WebClient = New WebClient
+
+    Sub StartUpdater() Handles Me.Load
+        Label1.Text = "Downloading: " & _passedText
+
+        'If My.Computer.FileSystem.FileExists(oldExe) Then My.Computer.FileSystem.DeleteFile(oldExe)
+        My.Computer.FileSystem.RenameFile(mainExe, "KSPMM.Old")
+
+        AddHandler client.DownloadProgressChanged, AddressOf client_ProgressChanged
+        AddHandler client.DownloadFileCompleted, AddressOf client_DownloadCompleted
+        client.DownloadFileAsync(New Uri(_passedText), mainExe)
+
+    End Sub
+    Sub CancelPressed() Handles Button1.Click
+        If (MsgBox("Are you sure?", MsgBoxStyle.YesNo, "Cancel") = MsgBoxResult.Yes) Then
+            Cancel = True
+            client.CancelAsync()
+        End If
+    End Sub
+
+    Private Sub client_ProgressChanged(ByVal sender As Object, ByVal e As DownloadProgressChangedEventArgs)
+        Dim bytesIn As Double = Double.Parse(e.BytesReceived.ToString())
+        Dim totalBytes As Double = Double.Parse(e.TotalBytesToReceive.ToString())
+        Dim percentage As Double = bytesIn / totalBytes * 100
+
+        ProgressBar1.Value = Int32.Parse(Math.Truncate(percentage).ToString())
+        Label1.Text = percentage.ToString("#0.00") & "% (" & BytesTO(bytesIn.ToString, KB) & "KB/" & BytesTO(totalBytes.ToString, KB) & "KB)"
+    End Sub
+
+    Private Sub client_DownloadCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.AsyncCompletedEventArgs)
+        If Cancel = True Then
+            MsgBox("Download Canceled")
+        Else
+            'My.Computer.FileSystem.DeleteFile(oldExe)
+            If (MsgBox("Download Complete! Do wish to start KSPMM now?", MsgBoxStyle.YesNo, "Completed!") = MsgBoxResult.Yes) Then
+                Process.Start(mainExe)
+                Application.Exit()
+            Else
+                Application.Exit()
+            End If
+        End If
+
+    End Sub
+
+    Const KB = 1
+    Const MB = 2
+    Const GB = 3
+    Const TB = 4
+    Function BytesTO(ByVal lBytes, ByVal convertto)
+
+        BytesTO = lBytes / (1024 ^ convertto)
+    End Function
+
+End Class
